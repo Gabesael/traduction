73d25631324d363dc6132bd66c51f88b212f4b33
cirill <cirill@asheart.fr>
2020-02-20 12:23:28 +0100

    20/02/2020

---
+++ fr/common.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'StaYouDescription' => 'StaYou est une application et un site web qui vous met en relation avec des commerçants, des médecins et des associations "LGBT+ Friendly"',
+
+    // =============== //
+
+    'link_home'  => 'accueil',
+    'link_home_' => 'l\'accueil',
+    'link_place' => 'lieux',
+    'link_shop'  => 'magasin',
+    'link_association' => 'association',
+
+    // =============== //
+
+    'descriptionPlace'       => 'Ici vous pouvez ajouter un lieu : comme une bibliothèque, ect...', // NOT USE FOR NOW
+    'descriptionShop'        => 'Ici vous pouvez ajouter un commerçant / magasin : comme un coiffeur, un bar, une boite de nuit. ',
+    'descriptionAssociation' => 'Ici vous pouvez ajouter une association / un espace public : comme une bibliothèque, une association LGBTQI+, PAEJ, Culturel, ect... ',
+
+    'callActionForm' => 'A vous de jouer',
+
+    // =============== //
+
+    'descriptionFeedback' => 'Un bug ? Une fautes ? Une idée ? Un question ?',
+
+    // =============== //
+
+    'thank_0' => 'Merci pour votre contribution, apres verification de notre part votre ajout apparaitra sur la carte. Vous pouvez revenir à',
+    'thank_1' => 'ou ajouter de nouveaux un nouveaux',
+    'thank_2' => 'ou une',
+
+    // =============== //
+
+    'mentionsLegales' => 'Mentions légales',
+    'CGU'             => 'CGU',
+    'CGV'             => 'CGV',
+
+];
